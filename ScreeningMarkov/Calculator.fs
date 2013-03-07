ef3d7428c87c2fdd101b9636dad0fb02e879dada
Ryan Roberts <ryansroberts@gmail.com>
2013-03-07 20:08:33 +0000

    Balmer peak exceeded

---
+++ ScreeningMarkov/Calculator.fs
@@ -0,0 +1,163 @@
+﻿namespace Kata
+module Calculator =
+    exception Empty
+    exception Error
+
+    type OperatorKey =
+    | Plus
+    | Minus
+    | Times
+    | Divide
+
+    type key = 
+    | NotKey
+    | AC
+    | Number of int
+    | Equals 
+    | OperatorKey of OperatorKey
+
+    type EnterNumber = int list
+
+    let formatNumber (num:EnterNumber) =
+        num
+            |> List.fold (fun acc i -> acc + i.ToString()) ""
+
+    let addDigit digit (num:EnterNumber) =
+        match digit,num,num.Length < 10 with 
+        | 0,[0],_    -> [0]           //No leading zeros can be entered
+        | n,[0],_    -> [n]           //Entering a digit removes the zero
+        | _,_,false  -> num           //No more than 10 digits
+        | _,_,_      -> num @ [digit]
+
+    let float (num:EnterNumber) =
+        0
+ 
+    type state = 
+    | EnterNumber of EnterNumber
+    | Operator of OperatorKey
+    | Result of float
+
+    let StartEnterNumber = EnterNumber [0]
+
+    type Calculator = Nil | Cons of (state * Calculator)
+    let calculator =  Nil 
+
+    let head calculator = 
+        match calculator with
+        | Nil -> raise Empty
+        | Cons(x,_) -> x
+
+    let tail calculator = 
+        match calculator with
+        | Nil -> raise Empty
+        | Cons(_,x) -> x
+
+
+    let calculate calculator = 
+        
+        let opFor operation = 
+            match operation with
+            | Plus   -> (+)
+            | Divide -> (/)
+            | Times  -> (*)
+            | Minus  -> (-)
+            
+        match calculator with
+        | Cons(EnterNumber rhs, Cons(Operator op,Cons(EnterNumber lhs,tail))) -> Nil
+        | _ -> Cons(StartEnterNumber,calculator)
+
+    let press key calculator = 
+        match key,calculator with
+        | AC,Nil                                -> Cons(StartEnterNumber,calculator)
+        | Number n, Cons(EnterNumber n2,_)      -> Cons(EnterNumber (addDigit n n2),(tail calculator))
+        | Equals  , Cons(_,_)                   -> calculate calculator 
+        | OperatorKey n, Cons(EnterNumber _,_)  -> Cons(StartEnterNumber,Cons(Operator n,(calculator)))
+        | _,_                                   -> calculator
+               
+    let presskeys (keys:key list) calculator = 
+        keys 
+          |> List.fold (fun acc k -> press k acc) calculator
+
+    let display calculator = 
+        match calculator with
+        | Nil                     -> ""
+        | Cons((EnterNumber n),_) -> sprintf "%s." (formatNumber n)
+        
+
+open Calculator
+open Xunit
+open Xunit.Extensions
+open Swensen.Unquote
+    
+
+
+module ``Entering integers``=
+    let oncalculator = calculator |> press AC
+    let numbers numbers = numbers |> List.map Number 
+
+    [<Fact>]
+    let ``Turn on the calculator`` () =
+        test <@ (calculator |> press AC) |> display = "0." @> 
+
+    [<Fact>]
+    let ``Pressing a single digit displays the digit`` ()=
+        test <@ oncalculator
+            |> press (Number 1) 
+            |> display = "1." @>
+
+    [<Fact>]
+    let ``Pressing two digits displays both`` ()=
+        test <@ oncalculator 
+            |> press (Number 1) 
+            |> press (Number 2) 
+            |> display = "12." @>
+
+    [<Fact>]
+    let ``Pressing 1 to 9 displays all entered digits`` ()=
+        test <@  oncalculator 
+            |> presskeys (numbers ([1..9] @ [0])) 
+            |> display = "1234567890." @>
+
+    [<Fact>]
+    let ``The display has a 10 digit limit`` ()=
+        test <@  oncalculator 
+            |> presskeys (numbers ([1..9] @ [0;1])) 
+            |> display = "1234567890." @>
+
+    [<Fact>]
+    let ``Pressing equals after a number starts entering a new number`` ()= 
+        test <@ oncalculator 
+                |> presskeys (numbers [1..3])
+                |> press Equals
+                |> press (Number 1)
+                |> display = "1." @>
+
+    let operators = [Equals;OperatorKey Plus;OperatorKey Minus;OperatorKey Times;OperatorKey Divide] |> List.toSeq
+    [<Theory>]
+    [<PropertyData("operators")>]
+    let ``Pressing an operator or equals after a number starts entering a new number`` (operator)= 
+        test <@ oncalculator 
+                |> presskeys (numbers [1..3])
+                |> press operator
+                |> press (Number 1)
+                |> display = "1." @>
+
+    [<Fact>]
+    let ``Cannot enter leading zeros`` ()=
+        test <@ oncalculator 
+                    |> presskeys (numbers [0;0;0])
+                    |> display = "0." @>
+                    
+
+module ``Integer math`` = 
+    let oncalculator = calculator |> press AC
+    let numbers numbers = numbers |> List.map Number 
+
+    [<Fact>] 
+    let ``Basic addition`` ()=
+        test <@ oncalculator
+                |> presskeys (numbers [1;2;3])
+                |> press     (OperatorKey Plus)
+                |> presskeys (numbers [4;5;6])
+                |> press     (Equals)
+                |> display = "579." @>
---
+++ ScreeningMarkov/Markov.fs
@@ -0,0 +1,104 @@
+﻿module ScreeningMarkup
+    open Xunit
+    open Swensen.Unquote
+
+    type symbol<'a when 'a:comparison> = 
+        | Start
+        | End
+        | Symbol of 'a
+
+    type Model<'a when 'a:comparison> = Map<symbol<'a>,Map<symbol<'a>,float>>
+
+    let DefaultModel = Map.empty
+
+    type Distribution<'a when 'a:comparison> = Map<symbol<'a>,float>
+    let distribution<'a when 'a:comparison> symbol (model:Model<'a>) = 
+        (match Map.tryFind symbol model with
+        | Some(frequencies) -> 
+            let k =  1.0 / (frequencies |> Map.toList |> List.sumBy snd)
+            frequencies |> Map.toList |> List.map (fun (s,f) -> (s,k*f)) |> Map.ofList
+        | None -> Map.empty)
+
+    let select r (distribution:Distribution<'a>) =
+        let rec find cur remaining =
+            match remaining with
+            | (s,f)::tail ->
+                if (r >= (cur + f)) then s
+                else find (cur + f) remaining.Tail
+            | _ -> End
+        find 0.0 (distribution |> Map.toList |> List.sortBy fst)
+
+    let defaults symbols =
+        [Start] @ symbols @ [End]
+
+    let NoSymbols<'a> = defaults []
+    let Symbols list = defaults (list |> List.map Symbol)
+
+    let updateProbabilties firstsymbol secondsymbol (model:Model<'a>) = 
+        match Map.tryFind firstsymbol model with
+        | Some(frequencies) -> 
+            match Map.tryFind secondsymbol frequencies with
+            | Some(f) -> model.Add (firstsymbol,frequencies.Add(secondsymbol,f + 1.0))
+            | None -> model.Add (firstsymbol,frequencies.Add(secondsymbol,1.0))
+        | None -> model.Add (firstsymbol,[(secondsymbol,1.0)] |> Map.ofList) 
+    
+    let rec train model symbols = 
+        match symbols with
+        | head::next::_ -> train model symbols.Tail |> updateProbabilties head next
+        | _ -> model
+
+
+    let rec enumerate<'a when 'a: comparison> (random:float seq) (model:Model<'a>)  = 
+        let sym = ref Start
+    
+        random 
+            |> Seq.map (fun r -> 
+                sym := select r (distribution sym.Value model)
+                sym.Value)
+            |> Seq.takeWhile ((<>) End)
+
+    [<Fact>]
+    let ``When we have no symbols the End symbol always follows the Start symbol`` ()=
+        test <@ train DefaultModel NoSymbols
+                |> Map.find Start |> Map.toList = [(End,1.0)] @>
+
+    [<Fact>]
+    let ``When we have a single symbol the probability it follows the start symbol is one`` ()=
+        test <@ train DefaultModel (Symbols ["a"])
+                |> Map.find Start |> Map.toList = [(Symbol("a"),1.0)] @>
+
+    [<Fact>]
+    let ``When we have a single symbol the probability it preceeds the end symbol is one`` ()=
+        test <@ train DefaultModel (Symbols ["a"])
+                |> Map.find (Symbol "a") |> Map.toList = [(End,1.0)]@>
+
+
+    [<Fact>]
+    let ``When we have symbols a b a c the probability of b or c following a is 1/2`` ()=
+        let trained = train DefaultModel (Symbols ["a";"b";"a";"c"])
+        test <@ trained  
+                |> distribution (Symbol "a")
+                |> Map.find (Symbol "b") = 0.5 @>
+        test <@ trained  
+                |> distribution (Symbol "a")
+                |> Map.find (Symbol "c") = 0.5 @>
+        test <@ trained  
+                |> distribution (Symbol "b")
+                |> Map.find (Symbol "a") = 1.0 @>
+        test <@ trained  
+                |> distribution (Symbol "c")
+                |> Map.find End = 1.0 @>
+
+    [<Fact>]
+    let ``Enumerating the model of symbols a b a c with 0 1/2 0 produces a b a`` ()=
+        let random = [0.0;0.5;0.0] |> List.toSeq
+
+        let trained = train DefaultModel (Symbols ["a";"b";"a";"c"])
+        test <@ trained |> enumerate (random) |> List.ofSeq = [Symbol("a");Symbol("b");Symbol("a")] @>
+
+    [<Fact>]
+    let ``Enumerating the model of symbols a b a c with 0 1 0 produces a c a`` ()=
+        let random = [0.0;1.0;0.0] |> List.toSeq
+
+        let trained = train DefaultModel (Symbols ["a";"b";"a";"c"])
+        test <@ trained |> enumerate (random) |> List.ofSeq = [Symbol("a");Symbol("c");Symbol("a")] @>
